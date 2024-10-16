7429507152bc07eb9cc7817efc8b68a978d0f140
s0ymigue <pemm080223hdfrnga2@cbtis217.edu.mx>
2024-10-16 08:19:36 +0200

    Creacion del proyecto base

---
+++ suma.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout<<"Introduce el primer valor"
+    cin>>a;
+    cout<<"Introduce el segundo valor"
+    cin>>b;
+
+return 0;
+}
