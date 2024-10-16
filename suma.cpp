51cc20829214c9247f6443f9cd20f8011d432a6d
s0ymigue <pemm080223hdfrnga2@cbtis217.edu.mx>
2024-10-16 08:35:39 +0200

    suma

--- suma.cpp
+++ suma.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main() {
-    int a, b;
+    int a, b, c;
     cout<<"Introduce el primer valor"
     cin>>a;
     cout<<"Introduce el segundo valor"
     cin>>b;
+    c=a+b;
 
 }
